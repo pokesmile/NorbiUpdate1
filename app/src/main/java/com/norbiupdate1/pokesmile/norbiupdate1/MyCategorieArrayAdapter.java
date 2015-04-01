7a8da6bdf7b91f9faf46d0b895de8838a2a3d7a9
Norbert Szalay <pokesmile87@gmail.com>
2015-04-01 14:21:14 +0200

    Working base functions

--- app/src/main/java/com/norbiupdate1/pokesmile/norbiupdate1/MyCategorieArrayAdapter.java
+++ app/src/main/java/com/norbiupdate1/pokesmile/norbiupdate1/MyCategorieArrayAdapter.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.List;
@@ -29,129 +30,128 @@ public class MyCategorieArrayAdapter extends ArrayAdapter<String>{
         View rowView = inflater.inflate(R.layout.categories_row_layout, parent, false);
         String categ = allValues.get(position);
 
-        TextView categorieImageTextView = (TextView) rowView.findViewById(R.id.categorie_picture);
+        ImageView categorieImageTextView = (ImageView) rowView.findViewById(R.id.categorie_picture);
         TextView categorieNameTextView = (TextView) rowView.findViewById(R.id.categorie_name);
 
         categorieNameTextView.setText(categ);
-        categorieImageTextView.setText(null);
 
         switch(categ){
             case "Italok":
-                categorieImageTextView.setBackgroundResource(R.drawable.drinks);
+                categorieImageTextView.setImageResource(R.drawable.drinks);
                 break;
             case "Csokoládék":
-                categorieImageTextView.setBackgroundResource(R.drawable.chocolates);
+                categorieImageTextView.setImageResource(R.drawable.chocolates);
                 break;
             case "Cukor hozzáadásával készült mártások":
-                categorieImageTextView.setBackgroundResource(R.drawable.sauces);
+                categorieImageTextView.setImageResource(R.drawable.sauces);
                 break;
             case "Desszertek, csokoládék, jégkrémek":
-                categorieImageTextView.setBackgroundResource(R.drawable.desserts);
+                categorieImageTextView.setImageResource(R.drawable.desserts);
                 break;
             case "Édesítőszerek":
-                categorieImageTextView.setBackgroundResource(R.drawable.sweeteners);
+                categorieImageTextView.setImageResource(R.drawable.sweeteners);
                 break;
             case "Egyéb":
-                categorieImageTextView.setBackgroundResource(R.drawable.others);
+                categorieImageTextView.setImageResource(R.drawable.others);
                 break;
             case "Feltétek – Hal":
-                categorieImageTextView.setBackgroundResource(R.drawable.fishes);
+                categorieImageTextView.setImageResource(R.drawable.fishes);
                 break;
             case "Feltétek – Marha, borjú":
-                categorieImageTextView.setBackgroundResource(R.drawable.beefs);
+                categorieImageTextView.setImageResource(R.drawable.beefs);
                 break;
             case "Feltétek – Sertés":
-                categorieImageTextView.setBackgroundResource(R.drawable.pig_meat);
+                categorieImageTextView.setImageResource(R.drawable.pig_meat);
                 break;
             case "Feltétek – Szárnyasok":
-                categorieImageTextView.setBackgroundResource(R.drawable.fried_chicken);
+                categorieImageTextView.setImageResource(R.drawable.fried_chicken);
                 break;
             case "Feltétek – Zöldség alapú feltét":
-                categorieImageTextView.setBackgroundResource(R.drawable.fried_vegetables);
+                categorieImageTextView.setImageResource(R.drawable.fried_vegetables);
                 break;
             case "Főételek – Főzelék":
-                categorieImageTextView.setBackgroundResource(R.drawable.lentils_stew);
+                categorieImageTextView.setImageResource(R.drawable.lentils_stew);
                 break;
             case "Főételek – Húsalapú":
-                categorieImageTextView.setBackgroundResource(R.drawable.meat_based_dishes);
+                categorieImageTextView.setImageResource(R.drawable.meat_based_dishes);
                 break;
             case "Főételek – Rizses":
-                categorieImageTextView.setBackgroundResource(R.drawable.main_courses_with_rice);
+                categorieImageTextView.setImageResource(R.drawable.main_courses_with_rice);
                 break;
             case "Főételek – Tésztás":
-                categorieImageTextView.setBackgroundResource(R.drawable.pastas);
+                categorieImageTextView.setImageResource(R.drawable.pastas);
                 break;
             case "Főételek – Zöldségalapú":
-                categorieImageTextView.setBackgroundResource(R.drawable.vegetable_dishes);
+                categorieImageTextView.setImageResource(R.drawable.vegetable_dishes);
                 break;
             case "Fűszerek, öntetek, kenyérrevalók":
-                categorieImageTextView.setBackgroundResource(R.drawable.spices);
+                categorieImageTextView.setImageResource(R.drawable.spices);
                 break;
             case "Gyümölcsök":
-                categorieImageTextView.setBackgroundResource(R.drawable.fruits);
+                categorieImageTextView.setImageResource(R.drawable.fruits);
                 break;
             case "Húsok, húskészítmények":
-                categorieImageTextView.setBackgroundResource(R.drawable.meats);
+                categorieImageTextView.setImageResource(R.drawable.meats);
                 break;
             case "Jégkrémek":
-                categorieImageTextView.setBackgroundResource(R.drawable.ice_creams);
+                categorieImageTextView.setImageResource(R.drawable.ice_creams);
                 break;
             case "Köretek – Burgonyaalapú":
-                categorieImageTextView.setBackgroundResource(R.drawable.fried_potatoes);
+                categorieImageTextView.setImageResource(R.drawable.fried_potatoes);
                 break;
             case "Köretek – Gabona alapú köretek":
-                categorieImageTextView.setBackgroundResource(R.drawable.dumplings);
+                categorieImageTextView.setImageResource(R.drawable.dumplings);
                 break;
             case "Köretek – Gyümölcs alapú köretek":
-                categorieImageTextView.setBackgroundResource(R.drawable.fruit_side_dishes);
+                categorieImageTextView.setImageResource(R.drawable.fruit_side_dishes);
                 break;
             case "Köretek – Rizs alapú köretek":
-                categorieImageTextView.setBackgroundResource(R.drawable.rice);
+                categorieImageTextView.setImageResource(R.drawable.rice);
                 break;
             case "Köretek – Tejtermék, tojás alapú köretek":
-                categorieImageTextView.setBackgroundResource(R.drawable.egg_salads);
+                categorieImageTextView.setImageResource(R.drawable.egg_salads);
                 break;
             case "Köretek – Tésztaköret":
-                categorieImageTextView.setBackgroundResource(R.drawable.spaghetti);
+                categorieImageTextView.setImageResource(R.drawable.spaghetti);
                 break;
             case "Levesek":
-                categorieImageTextView.setBackgroundResource(R.drawable.soups);
+                categorieImageTextView.setImageResource(R.drawable.soups);
                 break;
             case "Lisztek, tészták, pékáruk, gabonaipari termékek":
-                categorieImageTextView.setBackgroundResource(R.drawable.breads);
+                categorieImageTextView.setImageResource(R.drawable.breads);
                 break;
             case "Mártások":
-                categorieImageTextView.setBackgroundResource(R.drawable.sauces);
+                categorieImageTextView.setImageResource(R.drawable.sauces);
                 break;
             case "Nassok, olajos magvak":
-                categorieImageTextView.setBackgroundResource(R.drawable.snacks);
+                categorieImageTextView.setImageResource(R.drawable.snacks);
                 break;
             case "Pizzák":
-                categorieImageTextView.setBackgroundResource(R.drawable.pizzas);
+                categorieImageTextView.setImageResource(R.drawable.pizzas);
                 break;
             case "Saláták":
-                categorieImageTextView.setBackgroundResource(R.drawable.salads);
+                categorieImageTextView.setImageResource(R.drawable.salads);
                 break;
             case "Szendvicsek":
-                categorieImageTextView.setBackgroundResource(R.drawable.sandwiches);
+                categorieImageTextView.setImageResource(R.drawable.sandwiches);
                 break;
             case "Tejtermékek":
-                categorieImageTextView.setBackgroundResource(R.drawable.milk_products);
+                categorieImageTextView.setImageResource(R.drawable.milk_products);
                 break;
             case "Update készételek":
-                categorieImageTextView.setBackgroundResource(R.drawable.norbiupdate1icon);
+                categorieImageTextView.setImageResource(R.drawable.norbiupdate1icon);
                 break;
             case "Update1 Bisztrós termékek":
-                categorieImageTextView.setBackgroundResource(R.drawable.norbiupdate1icon);
+                categorieImageTextView.setImageResource(R.drawable.norbiupdate1icon);
                 break;
             case "Zöldség alapú köretek":
-                categorieImageTextView.setBackgroundResource(R.drawable.vegetable_side_dishes);
+                categorieImageTextView.setImageResource(R.drawable.vegetable_side_dishes);
                 break;
             case "Zöldségek, zöldségkészítmények, savanyúságok":
-                categorieImageTextView.setBackgroundResource(R.drawable.vegetables);
+                categorieImageTextView.setImageResource(R.drawable.vegetables);
                 break;
             default:
-                categorieImageTextView.setBackgroundResource(R.drawable.generalfood);
+                categorieImageTextView.setImageResource(R.drawable.generalfood);
                 break;
         }
 
